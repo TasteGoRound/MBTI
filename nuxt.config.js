441f7c3a9639f6d05724f848490c069313a7f46e
TasteGoRound <taste5115@gmail.com>
2020-06-26 23:36:39 +0900

    동적 라우팅 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,23 @@ if(process.env.npm_config_publish) {
   publish = {
     router: {
       base: '/MBTI/'
+    },
+    generate: {
+      routes: [
+        '/result/0',
+        '/result/1',
+        '/result/2',
+        '/result/3',
+        '/result/4',
+        '/result/5',
+        '/result/6',
+        '/result/7',
+        '/result/8',
+        '/result/9',
+        '/result/10',
+        '/result/11',
+        '/result/12',
+      ]
     }
   }
 }

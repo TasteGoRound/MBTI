1908c81de82724d66d94487293a6248c2a580981
TasteGoRound <taste5115@gmail.com>
2020-06-27 00:36:36 +0900

    데이터 수정

--- pages/result/_count.vue
+++ pages/result/_count.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="viewport">
-    <h2>{{ result.title }}</h2>
-    <p>{{ result.content }}</p>
-
-    <h3>[Turning Point]</h3>
-    <strong>{{ result.turningPoint.title }}</strong>
-    <p>{{ result.turningPoint.content }}</p>
+    <h1 class="promotion-title">BADGIRL LIFE STYLE</h1>
+    <h2 class="result">
+      당신의 남자친구가 성적으로 만족할 확률은
+      <em class="result-probability">{{ result.title }}</em> 입니다.</h2>
+    <div class="result-visualization"></div>
+    <p class="result-contents" v-html="result.content"></p>
+    <h3 class="turning-point">[Turning Point]</h3>
+    <strong class="turning-point-title">{{ result.turningPoint.title }}</strong>
+    <p class="turning-point-contents" v-html="result.turningPoint.content"></p>
   </div>
 </template>
 
@@ -36,5 +39,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.promotion-title {
+
+}
+
+.promotion-probability {}
+
 
 </style>

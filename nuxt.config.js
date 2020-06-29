114fb6bd85bcec6fad424b3a49e916a6d9d0b600
winterguard <aliasbonfire@gmail.com>
2020-06-29 23:10:55 +0900

    description 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '당신의 남자친구의 성적 만족도는?' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }

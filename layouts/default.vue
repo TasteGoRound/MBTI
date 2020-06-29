c22bfe559f6d8a2e6d175b8bc4dfde5f4d24ec76
winterguard <aliasbonfire@gmail.com>
2020-06-29 23:22:30 +0900

    og 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,11 @@ export default {
   head () {
     return {
       meta: [
-        { hid: 'og:image', property: 'og:image', content: '//' + location.host + ogImage }
+        { hid: 'og:image', property: 'og:image', content: '//' + location.host + ogImage },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'BADGIRLS LIFE STYLE' },
+        { hid: 'og:title', property: 'og:title', content: 'BADGIRLS LIFE STYLE' },
+        { hid: 'og:description', property: 'og:descriptions', content: '매력의 격이 다른 "배드걸 라이프 스타일"' }
       ]
     }
   }
